143d2791ea38ec3886a6f8c227c9f50223e9a94a
IlmanNashran <ilmannashran@gmail.com>
2023-08-27 14:58:41 +0800

    finish ui with button

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -18,14 +18,29 @@ class _LoginPageState extends State<LoginPage> {
     _deviceWidth = MediaQuery.of(context).size.width;
     return Scaffold(
       backgroundColor: Color.fromRGBO(125, 191, 211, 1.0),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        mainAxisSize: MainAxisSize.max,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          _avatarWidget(),
-          _emailFieldWidget(),
-        ],
+      body: Align(
+        alignment: Alignment.center,
+        child: Container(
+          height: _deviceHeight! * 0.60,
+          width: _deviceWidth,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              _avatarWidget(),
+              SizedBox(
+                height: _deviceHeight! * 0.05,
+              ),
+              _emailFieldWidget(),
+              _passwordFieldWidget(),
+              SizedBox(
+                height: _deviceHeight! * 0.10,
+              ),
+              _loginButton(),
+            ],
+          ),
+        ),
       ),
     );
   }
@@ -73,11 +88,11 @@ class _LoginPageState extends State<LoginPage> {
   }
 
   //create password field
-    Widget _passwordFieldWidget() {
+  Widget _passwordFieldWidget() {
     return Container(
       width: _deviceWidth! * 0.70,
       child: const TextField(
-        obscureText: true,//unseen text
+        obscureText: true, //unseen text
         cursorColor: Colors.white,
         autocorrect: false,
         style: TextStyle(
@@ -95,5 +110,25 @@ class _LoginPageState extends State<LoginPage> {
         ),
       ),
     );
+  }
+
+  Widget _loginButton() {
+    return MaterialButton(
+        minWidth: _deviceWidth! * 0.38,
+        height: _deviceHeight! * 0.05,
+        color: Colors.white,
+        child: const Text(
+          "LogIn",
+          style: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+            color: Color.fromRGBO(125, 191, 211, 1.0),
+          ),
+        ),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(25.0),
+          side: BorderSide(color: Colors.white),
+        ),
+        onPressed: () {}); //button material
   }
 }
